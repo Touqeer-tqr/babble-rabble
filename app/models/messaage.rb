1313d632089eb27ff50378db65dda80076556591
Touqeer-tqr <tqr093@gmail.com>
2017-11-20 21:21:00 +0500

    relationships defined

--- app/models/messaage.rb
+++ app/models/messaage.rb
@@ -1,2 +1,4 @@
 class Messaage < ApplicationRecord
+	belongs_to :chatroom
+	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+
+  has_many :messages
+  has_many :chatrooms, through: :messages
+  
 end
