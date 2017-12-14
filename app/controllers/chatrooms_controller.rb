b8c2d7106f02ea7c50cc97a2ced48e04a1c6763b
Touqeer-tqr <tqr093@gmail.com>
2017-12-14 16:50:25 +0500

    chatroom creation functionality implemented

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -6,4 +6,23 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.find_by_id(params[:id])
     @message = Message.new
   end
+
+  def create
+  	@chatroom = Chatroom.new(chatroom_params)
+    if @chatroom.save
+      respond_to do |format|
+        format.html { redirect_to root_path(), notice: 'chatroom created.' }
+      end
+    else
+      respond_to do |format|
+        format.js
+      end
+    end
+  end
+
+  private
+  def chatroom_params
+  	params.require(:chatroom).permit(:topic)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,7 @@ class UsersController < ApplicationController
   
   def home
   	@chatrooms = Chatroom.all
+    @chatroom = Chatroom.new if user_signed_in?
   end
-
-  def create
-    p '*'*100
-    user = User.new(user_params)
-    if user.save
-      redirect_to user_session_path
-    else
-      redirect_to signup_path, flash[:notice] =  user.errors.messages
-    end
-  end
-
-  private
-
-    def user_params
-      params.require(:user).permit(:email, :username, :password, :password_confirmation)
-    end
 
 end
